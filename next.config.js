f80590fd319418f82a8c6d7d389820deefaa656e
zgarnirawen <rawenzgarni1@gmail.com>
2025-09-10 20:58:35 +0100

    Fix Vercel deployment: Remove experimental optimizePackageImports causing client reference manifest error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 // Quick production optimization
 const nextConfig = {
-  experimental: {
-    optimizePackageImports: ['@clerk/nextjs', '@tanstack/react-query']
-  },
   images: {
     domains: ['images.clerk.dev', 'res.cloudinary.com'],
   },
